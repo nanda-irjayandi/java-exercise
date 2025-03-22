c9b280e0bd7ae423c5e54d42d95e260172b93b64
Nanda Irjayandi <nanda.irjayandi@gmail.com>
2025-03-22 16:17:32 +1100

    init: BinaryIntegerNode initial fields

---
+++ java-dsa/binarytree/src/BinaryIntegerNode.java
@@ -0,0 +1,21 @@
+public class BinaryIntegerNode {
+    public static void main(String[] args) {
+        BinaryIntegerNode test1 = new BinaryIntegerNode(4);
+        System.out.println(test1);
+    }
+    int value;
+    BinaryIntegerNode left;
+    BinaryIntegerNode right;
+
+    /**
+     * Constructor to create the first node without child
+     * @param value
+     */
+    BinaryIntegerNode(int value){
+        this.value = value;
+        left = null;
+        right = null;
+    }
+
+
+}
